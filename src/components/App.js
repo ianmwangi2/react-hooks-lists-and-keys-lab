8c2b6d93b752ec61c93d395698e70f33c9434398
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-07-31 18:55:04 +0300

    modifications

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import React from 'react';
+import NavBar from './NavBar';
+import ProjectList from './ProjectList';
 
-function App() {
+const projects = [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    technologies: ["Rails", "Bootstrap CSS"],
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    technologies: ["React", "Redux"],
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    technologies: ["React", "Redux", "Rails"],
+  },
+];
+
+const App = () => {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList projects={projects} />
     </div>
   );
-}
+};
 
 export default App;
